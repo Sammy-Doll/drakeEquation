1d70816a4ae05729bee2cd27405a7b24e7d50952
Sammy Doll <samuel.doll.2023@student.lhschools.org>
2020-02-20 13:50:36 -0500

    rStar input added

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -40,13 +40,25 @@ L = 1000 to 100,000,000 years (which will last somewhere between 1000 and 100,00
     // To Do: ask for f_l variable, store f_l variables, and commit
     // To Do: ask for f_i value and commit
     // To Do: ask for f_c value and commit
-    // To Do: ask for L valiue and commit
+    // To Do: ask for L value and commit
 
-        N = rStar * f_p * numberOfExoplanets * f_l * f_i * f_c * L;
+    N = rStar * f_p * numberOfExoplanets *f_l * f_i * f_c *L;
     System.out.println("The number of extraterrestrial species \n" +
-        "that can communicate with us is: " + N);
+                "that can communicate with us is: " + N);
 
+    System.out.println("AND I'M TELLIN' YA TO KEEP DAT YELLA 'HINNY A YOURS 'WAY FROM MY DAUGH-oh. Hi. Um... hold on a minute.");
+    System.out.println("*gunshot*");
+    System.out.println("Sorry, I didn't hear you come in. Thjnp must've sent ya for that whole 'Drake Equation' thing. Bunch a' nonsense, if ya ask me.");
+    System.out.println("But taint' mine to decide. Thjnp's a god friend. Just remember that, when yu leave here t'night, as far as anyone 'sides us is concerned, nothin' ilegal just happened.");
+    System.out.println("Ah, where are my manners. Names Vtgd, and I was literally created by some weird high-school kid for no reason other than to be weird while doing high-school things.");
+    System.out.println("Anyways, Thjnp asked me here to get different estimations of how many conquerable-I mean, alien species there are in this galaxy.");
+    System.out.println("I'm s'posed ta ask ya fur some values 'cause the estimation's made with some 'quation.");
+    System.out.println("Now, the first value, let's see here...");
+    System.out.println("*papers rustling");
+    System.out.println("A ha! Okay, give me a number divisible by three.");
+    rStar = klepto.nextInt();
 
+    
 
 
     }
